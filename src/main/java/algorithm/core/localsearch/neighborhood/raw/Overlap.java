4b3b75da79233a92ad626dc009a4c1319f5230d2
Dang Minh Nguyen <dangminh214@gmail.com>
2026-02-04 18:55:54 +0100

    add sample overlap.java

---
+++ src/main/java/algorithm/core/localsearch/neighborhood/raw/Overlap.java
@@ -0,0 +1,17 @@
+package algorithm.core.localsearch.neighborhood.raw;
+
+import algorithm.core.localsearch.neighborhood.generic.Neighborhood;
+import algorithm.solution.generic.Solution;
+
+import java.util.List;
+
+public class Overlap implements Neighborhood {
+    public Overlap() {
+
+    }
+
+    @Override
+    public Iterable getNeighbors(Solution initialSolution) {
+        return null;
+    }
+}
