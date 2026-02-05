e719338f3ad7bbf36c804b0d70c227c450b56f89
Dang Minh Nguyen <dangminh214@gmail.com>
2026-02-05 16:03:55 +0100

    delete unused interface

--- src/main/java/algorithm/core/localsearch/objective/generic/IterationAwareObjective.java
+++
@@ -1,7 +0,0 @@
-package algorithm.core.localsearch.objective.generic;
-
-import algorithm.solution.generic.Solution;
-
-public interface IterationAwareObjective<S extends Solution> extends Objective<S> {
-    void setIteration(int iteration, int maxIterations);
-}
